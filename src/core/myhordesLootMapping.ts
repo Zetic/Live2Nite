ffdcf4719d79d95bc9d68823c1e101dd1f112226
Zetic <105380590+Zetic@users.noreply.github.com>
2026-08-23 18:48:27 -0400

    Map Damaged Hacksaw normal loot

--- src/core/myhordesLootMapping.ts
+++ src/core/myhordesLootMapping.ts
@@ -60,6 +60,7 @@ export const MYHORDES_NORMAL_LOOT_MAPPING:Readonly<Partial<Record<string,MyHorde
   'bag_#00':mapped('plastic_bag'),
   'repair_kit_part_raw_#00':mapped('tool_bag'),
   'repair_one_#00':mapped('kwik_fix'),
+  'saw_tool_part_#00':mapped('saw_tool_part'),
   'lights_#00':mapped('box_of_matches'),
   'wire_#00':mapped('wire_reel'),
   'oilcan_#00':mapped('empty_oil_can'),
